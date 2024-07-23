abefc68f0e561f6b98674d8d5e2fd790d8c87d5d
chiw <wongchiking+github@gmail.com>
2024-07-23 21:07:21 +0800

    update i18n.js

--- src/lib/i18n.js
+++ src/lib/i18n.js
@@ -1,4 +1,7 @@
 import { createI18n } from "@inlang/paraglide-sveltekit";
 import * as runtime from "$lib/paraglide/runtime.js";
 
-export const i18n = createI18n(runtime);
+export const i18n = createI18n(runtime, {
+    // don't include the language or base path
+    exclude: ["/coffee-calculator"],
+});
