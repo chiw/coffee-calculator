8f1a07cbb51ad712589c87744a1ad9873d3d1a41
chiw <wongchiking+github@gmail.com>
2024-07-25 00:16:47 +0800

    fix some styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,34 +10,32 @@
 </script>
 
 <div class="m-3">
-    <h3 class="text-xl font-bold italic">{m.label_receipe_parameters()}</h3>
+    <div class="text-xl font-bold italic">{m.label_receipe_parameters()}</div>
 
-    <div class="flex flex-row">
+    <div class="flex flex-row mt-1">
         <div class="w-52 font-semibold">{m.label_coffee_bean()}</div>
         <button class="w-7 h-7 rounded-full border bg-slate-200 hover:bg-slate-300 font-bold" onclick={() => pourover.beanInGrams--}>-</button>
         <input class="ml-2 mr-1 border border-slate-200 max-w-10" bind:value={pourover.beanInGrams} />
-        <div>(g)</div>
+        <div class="mr-2">(g)</div>
         <button class="w-7 h-7 rounded-full border bg-slate-200 hover:bg-slate-300 font-bold" onclick={() => pourover.beanInGrams++}>+</button>
         
     </div>
-    <div class="flex flex-row">
+    <div class="flex flex-row mt-1">
         <div class="w-52 font-semibold">{m.label_coffee_to_water_ratio()}</div>
         <button class="w-7 h-7 rounded-full border bg-slate-200 hover:bg-slate-300 font-bold" onclick={() => pourover.coffeeToWaterRatio--}>-</button>
-        <div class="mx-2 border-none border-slate-200">1:</div>
+        <div class="mx-2 border-none border-slate-200 w-1">1:</div>
         <input class="mx-2 border border-slate-200 max-w-10" bind:value={pourover.coffeeToWaterRatio} />
-        <button class="w-7 h-7 rounded-full border bg-slate-200 hover:bg-slate-300 font-bold" onclick={() => pourover.coffeeToWaterRatio++}>+</button>
+        <button class="ml-1 w-7 h-7 rounded-full border bg-slate-200 hover:bg-slate-300 font-bold" onclick={() => pourover.coffeeToWaterRatio++}>+</button>
     </div>
 
-    <div class="flex flex-row">
+    <div class="flex flex-row mt-1">
         <div class="w-52 font-semibold">{m.label_water()}</div>
         <div>{pourover.waterInGrams}</div>
-        <div>(g)</div>
+        <div class="ml-1">(g)</div>
     </div>
 
-    <br/>
-
-    <div class="flex flex-row">
-        <div class="w-52 font-semibold">{m.label_recipe()}</div>
+    <div class="flex flex-row mt-3">
+        <div class="w-24 font-semibold">{m.label_recipe()}</div>
         <select class="border border-slate-200" bind:value={pourover.recipe}>
             {#each CofeeRecipesChoices as option}
                 <option value={option.id}>{option.displayLabelId}</option>
@@ -45,15 +43,10 @@
         </select>
     </div>
 
-
-    <br/>
-
-    <h3 class="text-xl font-bold italic">{m.label_steps()}</h3>    
+    <div class="text-xl font-bold italic mt-5">{m.label_steps()}</div>    
     <CoffeeRecipeStepsDisplay steps={pourover.coffeeRecipe.steps} />
 
-    <br/>
-
-    <h3 class="text-xl font-bold italic">{m.label_references()}</h3>
+    <div class="text-xl font-bold italic mt-5">{m.label_references()}</div>
     <div class="flex flex-col">
         {#if pourover.coffeeRecipe}
             {#each pourover.coffeeRecipe.references as reference, i}
