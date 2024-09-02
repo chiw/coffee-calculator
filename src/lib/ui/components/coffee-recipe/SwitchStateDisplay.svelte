689989aa67dbf344b8d278688ff7a84c86162f13
chiw <wongchiking+github@gmail.com>
2024-09-02 08:13:07 +0800

    update switch state display color

--- src/lib/ui/components/coffee-recipe/SwitchStateDisplay.svelte
+++ src/lib/ui/components/coffee-recipe/SwitchStateDisplay.svelte
@@ -8,13 +8,15 @@
 
 <div>
     {#if SwitchState.OPEN === switchState}
-        <div class=" w-12 font-bold text-xs { highlightState? "bg-indigo-500" : "bg-slate-300" } text-white text-center">
+        <!-- <div class=" w-12 font-bold text-xs { highlightState? "bg-indigo-500" : "bg-slate-300" } text-white text-center"> -->
+        <div class=" w-12 font-bold text-xs bg-white text-black text-center">
             {m.label_switchState_open()}
         </div>
     {/if}
 
     {#if SwitchState.CLOSED === switchState}
-        <div class=" w-12 font-bold text-xs { highlightState? "bg-rose-400" : "bg-slate-300" } text-white text-center">
+        <!-- <div class=" w-12 font-bold text-xs { highlightState? "bg-rose-400" : "bg-slate-300" } text-white text-center"> -->
+        <div class=" w-12 font-bold text-xs bg-black text-white text-center">
             {m.label_switchState_closed()}
         </div>
     {/if}
