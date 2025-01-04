861d58c80bda4fef2da95fbcb6c089d0a01458d6
chiw <wongchiking+github@gmail.com>
2025-01-04 12:24:44 +0800

    enhance 4:6 method config dialog style

--- src/lib/ui/components/coffee-recipe/Config46Method.svelte
+++ src/lib/ui/components/coffee-recipe/Config46Method.svelte
@@ -8,29 +8,97 @@
 </script>
 
 <div class="mt-4"></div>
-<div class="text-sm"><b>{m.label_46_method_40()}</b></div>    
+<div class="text-sm mb-2"><b>{m.label_46_method_40()}</b></div>    
 <div>
-    <label class="text-sm">
-        <input type="radio" name="pourRatios40Radio" bind:group={pourRatios40} value={Method46Flavor.STANDARD}>{m.label_46_method_40_choices_standard()}
-    </label>
-    <label class="text-sm">
-        <input type="radio" name="pourRatios40Radio" bind:group={pourRatios40} value={Method46Flavor.SWEETER}>{m.label_46_method_40_choices_sweeter()}
-    </label>
-    <label class="text-sm">
-        <input type="radio" name="pourRatios40Radio" bind:group={pourRatios40} value={Method46Flavor.BRIGHTER}>{m.label_46_method_40_choices_brighter()}
-    </label>
+    <fieldset class="flex flex-wrap gap-3">
+    <div>
+        <label class="flex cursor-pointer items-center justify-center rounded 
+            border border-gray-300 bg-white px-3 py-1 text-gray-900 
+            hover:border-gray-900 
+            has-[:checked]:border-black has-[:checked]:bg-black has-[:checked]:text-white"
+        >
+          <input type="radio" name="pourRatios40Radio" bind:group={pourRatios40} value={Method46Flavor.STANDARD} 
+            class="sr-only"
+          />
+    
+          <p class="text-sm font-bold">{m.label_46_method_40_choices_standard()}</p>
+        </label>
+    </div>
+
+    <div>
+        <label class="flex cursor-pointer items-center justify-center rounded 
+            border border-gray-300 bg-white px-3 py-1 text-gray-900 
+            hover:border-gray-900 
+            has-[:checked]:border-black has-[:checked]:bg-black has-[:checked]:text-white"
+        >
+          <input type="radio" name="pourRatios40Radio" bind:group={pourRatios40} value={Method46Flavor.SWEETER} 
+            class="sr-only"
+          />
+    
+          <p class="text-sm font-bold">{m.label_46_method_40_choices_sweeter()}</p>
+        </label>
+    </div>
+
+    <div>
+        <label class="flex cursor-pointer items-center justify-center rounded 
+            border border-gray-300 bg-white px-3 py-1 text-gray-900 
+            hover:border-gray-900 
+            has-[:checked]:border-black has-[:checked]:bg-black has-[:checked]:text-white"
+        >
+          <input type="radio" name="pourRatios40Radio" bind:group={pourRatios40} value={Method46Flavor.BRIGHTER} 
+            class="sr-only"
+          />
+    
+          <p class="text-sm font-bold">{m.label_46_method_40_choices_brighter()}</p>
+        </label>
+    </div>
+    </fieldset>
 </div>
 
 <div class="mt-4"></div>
-<div class="text-sm"><b>{m.label_46_method_60()}</b></div>
+<div class="text-sm mb-2"><b>{m.label_46_method_60()}</b></div>
 <div>
-    <label class="text-sm">
-        <input type="radio" name="pourRatios60Radio" bind:group={pourRatios60} value={Method46Concentration.LIGHTER}>{m.label_46_method_60_choices_lighter()}
-    </label>
-    <label class="text-sm">
-        <input type="radio" name="pourRatios60Radio" bind:group={pourRatios60} value={Method46Concentration.STRONGER}>{m.label_46_method_60_choices_stronger()}
-    </label>
-    <label class="text-sm">
-        <input type="radio" name="pourRatios60Radio" bind:group={pourRatios60} value={Method46Concentration.EVEN_STRONGER}>{m.label_46_method_60_choices_evenstronger()}
-    </label>
+    <fieldset class="flex flex-wrap gap-3">
+    <div>
+        <label class="flex cursor-pointer items-center justify-center rounded-md 
+            border border-gray-300 bg-white px-3 py-1 text-gray-900 
+            hover:border-gray-900 
+            has-[:checked]:border-black has-[:checked]:bg-black has-[:checked]:text-white"
+        >
+          <input type="radio" name="ColorOption" bind:group={pourRatios60} value={Method46Concentration.LIGHTER} 
+            class="sr-only"
+          />
+    
+          <p class="text-sm font-bold">{m.label_46_method_60_choices_lighter()}</p>
+        </label>
+    </div>
+
+    <div>
+        <label class="flex cursor-pointer items-center justify-center rounded-md 
+            border border-gray-300 bg-white px-3 py-1 text-gray-900 
+            hover:border-gray-900 
+            has-[:checked]:border-black has-[:checked]:bg-black has-[:checked]:text-white"
+        >
+          <input type="radio" name="ColorOption" bind:group={pourRatios60} value={Method46Concentration.STRONGER} 
+            class="sr-only"
+          />
+    
+          <p class="text-sm font-bold">{m.label_46_method_60_choices_stronger()}</p>
+        </label>
+    </div>
+
+    <div>
+        <label class="flex cursor-pointer items-center justify-center rounded-md 
+            border border-gray-300 bg-white px-3 py-1 text-gray-900 
+            hover:border-gray-900 
+            has-[:checked]:border-black has-[:checked]:bg-black has-[:checked]:text-white"
+        >
+          <input type="radio" name="ColorOption" bind:group={pourRatios60} value={Method46Concentration.EVEN_STRONGER} 
+            class="sr-only"
+          />
+    
+          <p class="text-sm font-bold">{m.label_46_method_60_choices_evenstronger()}</p>
+        </label>
+    </div>
+    </fieldset>
 </div>
--- src/lib/ui/components/coffee-recipe/Display46Method.svelte
+++ src/lib/ui/components/coffee-recipe/Display46Method.svelte
@@ -19,26 +19,26 @@
     <tbody>
         <tr>
             <td class="border border-gray-300">
-                <div class="text-sm">40%</div>
-                <div class="text-sm">{method46FlavorKey(pourRatios40)}</div>
+                <div class="text-sm font-bold ml-2">40%</div>
+                <div class="text-sm ml-2">{method46FlavorKey(pourRatios40)}</div>
             </td>            
             <td class="border border-gray-300">
                 {#if pourRatios40WaterInGrams}
                     {#each pourRatios40WaterInGrams as waterInGrams}
-                        <div class="bg-black text-white text-center rounded text-xs m-1 p-1 w-12"><b>{displayNumber(waterInGrams)}g</b></div>
+                        <div class="bg-black text-white text-center rounded text-xs m-1 p-1 w-12 font-bold">{displayNumber(waterInGrams)}g</div>
                     {/each}
                 {/if}                
             </td>
         </tr>
         <tr>
             <td class="border border-gray-300">
-                <div class="text-sm">60%</div>
-                <div class="text-sm">{Method46ConcentrationKey(pourRatios60)}</div>
+                <div class="text-sm font-bold ml-2">60%</div>
+                <div class="text-sm ml-2">{Method46ConcentrationKey(pourRatios60)}</div>
             </td>
             <td class="border border-gray-300">
                 {#if pourRatios60WaterInGrams}
                     {#each pourRatios60WaterInGrams as waterInGrams}
-                    <div class="bg-black text-white text-center rounded text-xs m-1 p-1 w-12"><b>{displayNumber(waterInGrams)}g</b></div>
+                    <div class="bg-black text-white text-center rounded text-xs m-1 p-1 w-12 font-bold">{displayNumber(waterInGrams)}g</div>
                     {/each}
                 {/if}
             </td>
