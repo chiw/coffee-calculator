ff3430c115b81b1153b7f7f910094a0d74f3e811
chiw <wongchiking+github@gmail.com>
2025-02-22 23:02:23 +0800

    remove usage of dirtyFixFullUrlPath

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -5,18 +5,14 @@ export const trailingSlash = 'never';
 import { i18n } from '$lib/i18n.js';
 import { base } from '$app/paths';
 import { redirect } from '@sveltejs/kit';
-import { dirtyFixFullUrlPath } from '$lib/utils/url.js';
 
 export const load = ({ params }) => {
     console.log('page load', params);
 
-    let path = base + "/recipes/";
+    const path = base + '/recipes';
     console.log('root pages path', path);
 
-    path = dirtyFixFullUrlPath(path);
-    console.log('root pages path after dirty fix', path);
-
-    let i18nResolveRoute = i18n.resolveRoute(path);
+    const i18nResolveRoute = i18n.resolveRoute(path);
     console.log('requires to redirect', path, i18nResolveRoute);
 
     redirect(302, i18nResolveRoute);
