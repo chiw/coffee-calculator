99b8ee940e93d4a3bf8b8a3076d4025a0c64a1ba
chiw <wongchiking+github@gmail.com>
2025-02-22 18:19:26 +0800

    dirty fix url.ts for github pages build

--- src/lib/utils/url.ts
+++ src/lib/utils/url.ts
@@ -4,9 +4,18 @@ export function getFullUrl(path: string): string {
     console.log('getFullUrl path', path);
     const hostname = typeof window !== 'undefined' ? window.location.origin : '';
     const cleanPath = path.startsWith('/') ? path : `/${path}`;
+    const dirtyFixedCleanPath = dirtyFixForGithubPages(cleanPath, base);
     const basePath = base || '';
-    const fullUrl = `${hostname}${basePath}${cleanPath}`;
-    console.log('hostname', hostname, 'cleanPath', cleanPath, 'basePath', basePath);
+    // const fullUrl = `${hostname}${basePath}${cleanPath}`;
+    const fullUrl = `${hostname}${basePath}${dirtyFixedCleanPath}`;
+    console.log('hostname', hostname, 'dirtyFixedCleanPath', dirtyFixedCleanPath, 'basePath', basePath);
     console.log('fullUrl', fullUrl);
     return fullUrl;
+}
+
+const dirtyFixForGithubPages = (cleanPath: string, basePath: string): string => {
+    if (basePath === '..' && cleanPath.startsWith('/coffee-calculator')) {
+        return cleanPath.replace('/coffee-calculator', '');
+    }
+    return cleanPath;
 }
