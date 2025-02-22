7d94ef657d9bb77555e94fa5f1d482c8fd514dce
chiw <wongchiking+github@gmail.com>
2025-02-22 18:03:12 +0800

    print getFullUrl path for build debugging

--- src/lib/utils/url.ts
+++ src/lib/utils/url.ts
@@ -1,6 +1,7 @@
 import { base } from '$app/paths';
 
 export function getFullUrl(path: string): string {
+    console.log('getFullUrl path', path);
     const hostname = typeof window !== 'undefined' ? window.location.origin : '';
     const cleanPath = path.startsWith('/') ? path : `/${path}`;
     const basePath = base || '';
